5a8b5f5d3b82ea65aebe314e08615dd29bd7201b
mintnguyen.pa <mintnguyen.pa@gmail.com>
2022-04-10 21:38:10 -0700

    feat: addMarker

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,10 @@
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -8,6 +15,24 @@ export class CustomMap {
         lat: 0,
         lng: 0,
       },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: "Hi",
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,8 @@ import { CustomMap } from "./CustomMap";
 import { User } from "./User";
 
 const user = new User();
-console.log(user);
-
 const company = new Company();
-console.log(company);
+const customMap = new CustomMap("map");
 
-new CustomMap("map");
+customMap.addMarker(user);
+customMap.addMarker(company);
